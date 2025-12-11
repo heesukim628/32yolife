96f545519e266519306408decb563e2935861d3f
heesukim628 <heesukim628@gmail.com>
2025-12-10 19:48:26 -0500

    Title and description

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Decision to Leave',
+  author: '김희수',
+  headerTitle: 'Decision to Leave',
+  description: '짧게만 추억하길',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
