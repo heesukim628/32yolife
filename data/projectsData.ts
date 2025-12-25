3a63214fe8d50bd35de8e6c5c7ae2e1228922ae4
heesukim628 <heesukim628@gmail.com>
2025-12-25 06:13:16 -0500

    add liked music playlist to project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,21 +7,11 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'Heesu\'s Liked Music',
+    description: ``,
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+    href: 'https://music.youtube.com/playlist?list=PLi2rqdJeAsjuVN0xZFXIxLBHwNeTBCSGB&si=5A0VQZkIvrY9xjyl',
+  }
 ]
 
 export default projectsData
